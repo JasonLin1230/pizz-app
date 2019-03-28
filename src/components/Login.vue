7114b2350831f2341d81668fd462ac2011f1365b
Jason Lin <JasonLin1230@163.com>
2019-03-28 22:14:38 +0800

    login&register

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,38 @@
 <template>
-    <h2>登录</h2>
+    <div class="row mt-3">
+        <div class="col-md-12 col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <img src="../assets/login.png" alt="" class="mx-auto d-block">
+                    <form @submit.prevent="onSubmit">
+                        <div class="form-group">
+                            <label for="email">邮箱</label>
+                            <input type="eamil" v-model="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">密码</label>
+                            <input type="password" v-model="password" class="form-control">
+                        </div>
+                        <button class="btn btn-block btn-success">登录</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            email:'',
+            password:'',
+        }
+    },
+    methods:{
+        onSubmit (){
+
+        }
+    }
+}
+</script>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,3 +1,43 @@
 <template>
-    <h2>注册</h2>
+    <div class="row mt-3">
+        <div class="col-md-12 col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <img src="../assets/register.png" alt="" class="mx-auto d-block">
+                    <form @submit.prevent="onSubmit">
+                        <div class="form-group">
+                            <label for="email">邮箱</label>
+                            <input type="eamil" v-model="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">密码</label>
+                            <input type="password" v-model="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="confirm-password">确认密码</label>
+                            <input type="password" v-model="confirmPassword" class="form-control">
+                        </div>
+                        <button class="btn btn-block btn-success">注册</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            email:'',
+            password:'',
+            confirmPassword:''
+        }
+    },
+    methods:{
+        onSubmit (){
+
+        }
+    }
+}
+</script>
